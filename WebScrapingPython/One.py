a7e4f68dd4c73c25088303b3191cb75bb8ec4727
Daniel Ortega Bello <daob.2396@gmail.com>
2025-09-12 15:36:04 -0500

    Pruebas platzi

--- WebScrapingPython/One.py
+++ WebScrapingPython/One.py
@@ -8,12 +8,23 @@ response = requests.get(url)
 
 print(response.status_code)
 
-
 # Status code 200 means the request was successful 
-#status code 404 means the page was not found
+#status code 404 means the page was not found Error
 #status code 500 means there is an internal server error
 #status code 403 means access is forbidden
 #status code 301 means the page has been moved permanently 
 
 print(response.text) #Returns the HTML content of the page
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, 'html.parser')
+    #print(soup.prettify()) #Prettifies the HTML content
+
+    head = soup.find('title')
+    print(head.text) #Prints the title of the page
+else:
+    print('Error al realizar la solicitud')
+
+
+   
 
